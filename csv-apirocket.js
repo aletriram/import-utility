c2f03b18c2efb9b095437c1b28187fd2aeff9424
Alejandro <atrinidad@existo.es>
2020-09-01 14:58:03 +0200

    feat: incluido axios

--- csv-apirocket.js
+++ csv-apirocket.js
@@ -1,13 +1,23 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const { jsonToGraphQLQuery, EnumType } = require('json-to-graphql-query');
+const axios = require('axios');
 
-const name = 'NOTICIAS.csv'
+const filename = 'NOTICIAS.csv'
 const APIKEY = '';
+const APIURL = '';
 
-return true;
-fs.createReadStream(`tmp/${name}`).pipe(csv()).on('data', (row) => {
-	console.log(row);
+fs.createReadStream(`tmp/${filename}`).pipe(csv()).on('data', (row) => {
+
+	let query = {
+
+	}
+
+	return axios.post('', { query: jsonToGraphQLQuery(query) }, config).then((res) => {
+
+	}).catch(error => {
+		console.log(error.message);
+	});
 }).on('end', () => {
 	console.log('CSV file successfully processed');
 });
